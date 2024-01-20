2fbb8c5b4cb9c72f8f8d5361889cc86e2ec6185b
smritipradhan <smritipradhan545@gmail.com>
2024-01-20 15:36:29 +0530

    Mongo Changes

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 import express, { json } from "express";
 import testRouter from "./routes/testRoute.js";
-import mongoose from "mongoose";
 import { config } from "dotenv";
 import cors from "cors";
 
@@ -12,11 +11,5 @@ app.use(cors()); // For preventing CORS Error
 app.use(json());
 
 app.use("/api/v1", testRouter);
-
-const DB = process.env.DATABASE;
-
-mongoose.connect(DB).then((con) => {
-  console.log("Connected Succesfully !!");
-});
 
 export default app;
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 import app from "./app.js";
 import { config } from "dotenv";
+import mongoose from "mongoose";
 
 config({ path: "./config.env" });
 
 const PORT = process.env.PORT || 3000;
+
+const DB = process.env.DATABASE;
+
+mongoose.connect(DB).then((con) => {
+  console.log("Connected Succesfully !!");
+});
 
 app.listen(PORT, () => {
   console.log(`Listening at Port ${PORT}`);
